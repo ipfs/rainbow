5644135d8ccebc09d95cbc3a9a4d1701af253169
Henrique Dias <mail@hacdias.com>
2024-02-15 08:51:39 +0100

    test: add iffy test

---
+++ gc_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"testing"
+
+	"github.com/ipfs/go-cid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPeriodicGC(t *testing.T) {
+	t.Parallel()
+
+	_, gnd := mustTestServer(t, Config{})
+
+	cids := []cid.Cid{
+		mustAddFile(t, gnd, []byte("a")),
+		mustAddFile(t, gnd, []byte("b")),
+		mustAddFile(t, gnd, []byte("c")),
+		mustAddFile(t, gnd, []byte("d")),
+		mustAddFile(t, gnd, []byte("e")),
+		mustAddFile(t, gnd, []byte("f")),
+	}
+
+	for i, cid := range cids {
+		has, err := gnd.blockstore.Has(context.Background(), cid)
+		assert.NoError(t, err, i)
+		assert.True(t, has, i)
+	}
+
+	// NOTE: ideally, we'd be able to spawn an isolated Rainbow instance with the
+	// periodic GC settings configured (interval, threshold). The way it is now,
+	// we can only test if the periodicGC function and the GC function work, but
+	// not if the timer is being correctly set-up.
+	//
+	// Tracked in https://github.com/ipfs/rainbow/issues/89
+	err := gnd.periodicGC(context.Background(), 1)
+	require.NoError(t, err)
+
+	for i, cid := range cids {
+		has, err := gnd.blockstore.Has(context.Background(), cid)
+		assert.NoError(t, err, i)
+		assert.False(t, has, i)
+	}
+}
