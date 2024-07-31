adc6960ade3bea3809e25850bc15c2c0844c1474
Adin Schmahmann <adin.schmahmann@gmail.com>
2024-07-31 18:17:28 -0400

    feat: increase routing limits

--- setup_bitswap.go
+++ setup_bitswap.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"github.com/ipfs/boxo/routing/providerquerymanager"
+	"github.com/libp2p/go-libp2p/core/peerstore"
 	"time"
 
 	"github.com/ipfs/boxo/bitswap"
@@ -23,6 +25,12 @@ import (
 
 func setupBitswapExchange(ctx context.Context, cfg Config, h host.Host, cr routing.ContentRouting, bstore blockstore.Blockstore) exchange.Interface {
 	bsctx := metri.CtxScope(ctx, "ipfs_bitswap")
+	n := &providerQueryNetwork{cr, h}
+	pqm, err := providerquerymanager.New(ctx, n, providerquerymanager.WithMaxInProcessRequests(100))
+	if err != nil {
+		panic(err)
+	}
+	cr = &wrapProv{pqm: pqm}
 	bn := bsnet.NewFromIpfsHost(h, cr)
 
 	// --- Client Options
@@ -32,6 +40,14 @@ func setupBitswapExchange(ctx context.Context, cfg Config, h host.Host, cr routi
 	rebroadcastDelay := delay.Fixed(10 * time.Second)
 	// bitswap.ProviderSearchDelay: default is 1 second.
 	providerSearchDelay := 1 * time.Second
+
+	// --- Bitswap Client Options
+	clientOpts := []bsclient.Option{
+		bsclient.RebroadcastDelay(rebroadcastDelay),
+		bsclient.ProviderSearchDelay(providerSearchDelay),
+		bsclient.WithoutDuplicatedBlockStats(),
+		bsclient.WithDefaultLookupManagement(false),
+	}
 
 	// If peering and shared cache are both enabled, we initialize both a
 	// Client and a Server with custom request filter and custom options.
@@ -50,35 +66,86 @@ func setupBitswapExchange(ctx context.Context, cfg Config, h host.Host, cr routi
 			return ok
 		}
 
-		// Initialize client+server
-		bswap := bitswap.New(bsctx, bn, bstore,
-			// --- Client Options
-			bitswap.RebroadcastDelay(rebroadcastDelay),
-			bitswap.ProviderSearchDelay(providerSearchDelay),
-			bitswap.WithoutDuplicatedBlockStats(),
+		// turn bitswap clients option into bitswap options
+		var opts []bitswap.Option
+		for _, o := range clientOpts {
+			opts = append(opts, bitswap.WithClientOption(o))
+		}
 
-			// ---- Server Options
+		// ---- Server Options
+		opts = append(opts,
 			bitswap.WithPeerBlockRequestFilter(peerBlockRequestFilter),
 			bitswap.ProvideEnabled(false),
 			// Do not keep track of other peer's wantlists, we only want to reply if we
 			// have a block. If we get it later, it's no longer relevant.
 			bitswap.WithPeerLedger(&noopPeerLedger{}),
 			// When we don't have a block, don't reply. This reduces processment.
-			bitswap.SetSendDontHaves(false),
-		)
+			bitswap.SetSendDontHaves(false))
+
+		// Initialize client+server
+		bswap := bitswap.New(bsctx, bn, bstore, opts...)
 		bn.Start(bswap)
 		return &noNotifyExchange{bswap}
 	}
 
 	// By default, rainbow runs with bitswap client alone
-	bswap := bsclient.New(bsctx, bn, bstore,
-		// --- Client Options
-		bsclient.RebroadcastDelay(rebroadcastDelay),
-		bsclient.ProviderSearchDelay(providerSearchDelay),
-		bsclient.WithoutDuplicatedBlockStats(),
-	)
+	bswap := bsclient.New(bsctx, bn, bstore, clientOpts...)
 	bn.Start(bswap)
 	return bswap
+}
+
+type providerQueryNetwork struct {
+	routing.ContentRouting
+	host.Host
+}
+
+func (p *providerQueryNetwork) ConnectTo(ctx context.Context, id peer.ID) error {
+	return p.Host.Connect(ctx, peer.AddrInfo{ID: id})
+}
+
+func (p *providerQueryNetwork) FindProvidersAsync(ctx context.Context, c cid.Cid, i int) <-chan peer.ID {
+	out := make(chan peer.ID, i)
+	go func() {
+		defer close(out)
+		providers := p.ContentRouting.FindProvidersAsync(ctx, c, i)
+		for info := range providers {
+			if info.ID == p.Host.ID() {
+				continue // ignore self as provider
+			}
+			p.Host.Peerstore().AddAddrs(info.ID, info.Addrs, peerstore.TempAddrTTL)
+			select {
+			case <-ctx.Done():
+				return
+			case out <- info.ID:
+			}
+		}
+	}()
+	return out
+}
+
+type wrapProv struct {
+	pqm *providerquerymanager.ProviderQueryManager
+}
+
+var _ routing.ContentRouting = (*wrapProv)(nil)
+
+func (r *wrapProv) Provide(ctx context.Context, c cid.Cid, b bool) error {
+	return routing.ErrNotSupported
+}
+
+func (r *wrapProv) FindProvidersAsync(ctx context.Context, c cid.Cid, _ int) <-chan peer.AddrInfo {
+	retCh := make(chan peer.AddrInfo)
+	go func() {
+		defer close(retCh)
+		provsCh := r.pqm.FindProvidersAsync(ctx, c)
+		for p := range provsCh {
+			select {
+			case retCh <- peer.AddrInfo{ID: p}:
+			case <-ctx.Done():
+			}
+		}
+	}()
+	return retCh
 }
 
 type noopPeerLedger struct{}
